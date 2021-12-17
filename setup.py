051a4d3e81c824128bad898b80c9662e28f8f590
Sudeep Batra <sudeepbatra@yahoo.com>
2021-12-17 20:23:54 +0000

    python 3.7 to 3.8

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ DESCRIPTION = 'A python based web load testing tool.'
 URL = 'https://github.com/me/myproject'
 EMAIL = 'me@example.com'
 AUTHOR = 'Sudeep Batra'
-REQUIRES_PYTHON = '>=3.7.0'
+REQUIRES_PYTHON = '>=3.8.0'
 VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
@@ -120,7 +120,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
